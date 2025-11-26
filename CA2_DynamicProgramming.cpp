2e28b8c51387f007057570d35d13f30694fe65f9
Priyanka <priyankakwambokaachoki@gmail.com>
2025-11-26 21:42:55 +0000

    Add the memoization code

--- CA2_DynamicProgramming.cpp
+++ CA2_DynamicProgramming.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int fibRecursive(int n);
+int fibRecursion(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	return fibRecursion(n - 1) + fibRecursion(n - 2);
+}
+
+int fibMemo(int n, vector<int>& memo) {
+	if (n <= 1) {
+		return n;
+	}
+	if (memo[n] != -1) {    
+		return memo[n];
+	}
+
+	memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+	return memo[n];
+}
 
 int main() {
 	int n = 10;
-	cout << "fibRecursive(" << n << ") = " << fibRecursive(n) << endl;
-	return 0;
-}
 
+	// Test for normal recursion
+	cout << "Fibonacci Recursive :" << fibRecursion(n) << endl;
+
+	//Test memoization
+	vector<int> memo(n + 1, -1);
+	cout << "Fibonacci Memozation :" << fibMemo(n, memo) << endl;
+
+	return 0; 
+}
--- fibonacciRecursion.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-
-int fibRecursive(int n) {
-	if (n <= 1) return n;
-	return fibRecursive(n - 1) + fibRecursive(n - 2);
-}
