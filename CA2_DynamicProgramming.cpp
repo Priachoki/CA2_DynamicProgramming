bb393cd1fe0d52add40e492125dbfe29bc9600ef
Priyanka <priyankakwambokaachoki@gmail.com>
2025-11-27 00:16:21 +0000

    Add Tabulation code

--- CA2_DynamicProgramming.cpp
+++ CA2_DynamicProgramming.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+
+// Fibonacci - Recursion
 int fibRecursion(int n) {
 	if (n <= 1) {
 		return n;
@@ -10,6 +12,7 @@ int fibRecursion(int n) {
 	return fibRecursion(n - 1) + fibRecursion(n - 2);
 }
 
+// Fibonacci - Memoization
 int fibMemo(int n, vector<int>& memo) {
 	if (n <= 1) {
 		return n;
@@ -22,6 +25,27 @@ int fibMemo(int n, vector<int>& memo) {
 	return memo[n];
 }
 
+
+// Fibonacci - Tabulation
+int fibTab(int n) {
+	if (n <= 1) {
+		return n;
+
+	}
+	vector<int> dp(n + 1);
+
+	dp[0] = 0;
+	dp[1] = 1;
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+
+	}
+	return dp[n];
+	
+}
+
+
 int main() {
 	int n = 10;
 
@@ -31,6 +55,9 @@ int main() {
 	//Test memoization
 	vector<int> memo(n + 1, -1);
 	cout << "Fibonacci Memozation :" << fibMemo(n, memo) << endl;
+
+	//Test Tabulation
+	cout << "Fibonacci Tabulation: " << fibTab(n) << endl;
 
 	return 0; 
 }
